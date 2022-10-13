7680b7ec8b991773603f6bccef696df3f074439d
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2022-10-13 15:55:05 +1100

    Add start scene (menu)

---
+++ Assets/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Map1Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Map2Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Map3Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+}
