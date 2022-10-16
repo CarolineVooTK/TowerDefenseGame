2b7942e109c40b3b9d60d9152024baa41593f841
cvoo <cvoo@student.unimelb.edu.au>
2022-10-17 01:44:17 +1100

    Revert "Update Map_1.unity"
    This reverts commit abbe68abf7c4928e6ff900faa78d2f8f9591095a.

--- Assets/Scripts/UI/PanelTransition.cs
+++ Assets/Scripts/UI/PanelTransition.cs
@@ -23,7 +23,7 @@ public class PanelTransition : MonoBehaviour
     private IEnumerator Wait()
     {
         yield return new WaitForSeconds(1f);
-        Time.timeScale = 0f;
+        Time.timeScale = 0.1f;
     }
 
     public void SwitchTo(RectTransform panel)
