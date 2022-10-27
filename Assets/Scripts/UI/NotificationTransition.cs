e2b3a7ed35083829bcd5d31a34a8c92d081bb43e
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2022-10-28 02:00:29 +1100

    Fixed notification glitch

---
+++ Assets/Scripts/UI/NotificationTransition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// Class to turn off all notifications
+public class NotificationTransition : MonoBehaviour
+{
+    [SerializeField] private RectTransform[] panels;
+
+    // Only set first panel as active
+    public void TurnOffNotif()
+    {
+        // Deactivate all panels
+        foreach (var panel in panels)
+            panel.gameObject.SetActive(false);
+
+    }
+}
